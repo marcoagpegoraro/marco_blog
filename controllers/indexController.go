5e70932593ec7f15b7258883605eb71680bbd27d
Marco Antonio <marco@Marcos-MacBook-Air.local>
2023-09-30 21:16:40 -0300

    list posts working

--- controllers/indexController.go
+++ controllers/indexController.go
@@ -1,9 +1,27 @@
 package controllers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/marcoagpegoraro/marco_blog/initializers"
+	"github.com/marcoagpegoraro/marco_blog/models"
+)
 
 func GetIndex(c *fiber.Ctx) error {
+
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+
+	// fmt.Println(posts)
+
+	for index, _ := range posts {
+		fmt.Println(index)
+		// fmt.Println(element)
+	}
+
 	return c.Render("index/index", fiber.Map{
 		"Title": "Home",
+		"Posts": posts,
 	}, "layouts/main")
 }
