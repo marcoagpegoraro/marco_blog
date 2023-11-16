8cd9bd254ab71d27e1b78b7c33bbeec62b07fed8
Marco Antonio <marco@Marcos-MacBook-Air.local>
2023-11-16 19:50:37 -0300

    tailwind css, navbar migrated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/*.django"],
+  content: [
+    "./views/*.django", 
+    "./views/**/*.django"
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#305c96",
+          "secondary": "#8199d3",
+          "accent": "#016568",
+          "neutral": "#1c191f",
+          "base-100": "#424153",
+          "info": "#afc2e4",
+          "success": "#1a7a70",
+          "warning": "#a3730a",
+          "error": "#f54d7c",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 }
 
